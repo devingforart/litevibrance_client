de20e7f58721ce801604b4c0fc58addb3f2d4d62
ldelgado <devingforart@gmail.com>
2025-02-01 01:21:47 -0300

    footer

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ const App: React.FC = () => {
       <FloatingThemeSwitch />
       <main>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home />} /> 
           <Route path="/products" element={<ProductList />} />
           <Route path="/products/:id" element={<ProductDetail />} />
           <Route path="/cart" element={<Cart />} />
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,7 +5,7 @@ import './Footer.scss';
 const Footer: React.FC = () => {
   return (
     <footer className="footer">
-      <p>&copy; {new Date().getFullYear()} MiTienda. Todos los derechos reservados.</p>
+      <p>&copy; {new Date().getFullYear()} liteVibrance. Todos los derechos reservados.</p>
     </footer>
   );
 };
