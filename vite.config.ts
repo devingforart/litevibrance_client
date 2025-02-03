1f936be7c0679522564af73440f27ad782964dde
ldelgado <devingforart@gmail.com>
2025-02-03 10:13:06 -0300

    cssCodeSplit

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    cssCodeSplit: false, // Esto unirá todo el CSS en un único archivo
+  },
   server: {
+    // configuración del servidor en desarrollo (si la necesitas)
   },
 });
